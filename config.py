c4b60f5be19b44b020e34cb60d465bb3051ee311
Shadwoo <looktouhid@gmail.com>
2025-12-11 12:08:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -88,10 +88,10 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://files.catbox.moe/qp0aiy.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png"
+    "PING_IMG_URL", "https://files.catbox.moe/no21b2.jpg"
 )
 PLAYLIST_IMG_URL = "https://graph.org//file/3dfcffd0c218ead96b102.png"
-STATS_IMG_URL = "https://graph.org//file/99a8a9c13bb01f9ac7d98.png"
+STATS_IMG_URL = "https://files.catbox.moe/760khf.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
