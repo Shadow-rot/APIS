f9537c52bf049004f6525b9041079645ecb913f9
Shadwoo <looktouhid@gmail.com>
2025-12-11 12:26:26 +0530

    Update help.py

--- AviaxMusic/utils/inline/help.py
+++ AviaxMusic/utils/inline/help.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from AviaxMusic import app
 
 
-def help_pannel(_, START: Union[bool, int] = None):
+def help_pannel(_, START: Union[bool, int] = None, page: int = 1):
     first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
     second = [
         InlineKeyboardButton(
@@ -14,81 +14,134 @@ def help_pannel(_, START: Union[bool, int] = None):
         ),
     ]
     mark = second if START else first
-    upl = InlineKeyboardMarkup(
-        [
+    
+    if page == 1:
+        upl = InlineKeyboardMarkup(
             [
-                InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
-            ],
+                [
+                    InlineKeyboardButton(
+                        text=_["H_B_1"],
+                        callback_data="help_callback hb1",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_2"],
+                        callback_data="help_callback hb2",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_3"],
+                        callback_data="help_callback hb3",
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        text=_["H_B_4"],
+                        callback_data="help_callback hb4",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_5"],
+                        callback_data="help_callback hb5",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_6"],
+                        callback_data="help_callback hb6",
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Next",
+                        callback_data="help_page 2",
+                    ),
+                ],
+                mark,
+            ]
+        )
+    
+    elif page == 2:
+        upl = InlineKeyboardMarkup(
             [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
-            ],
+                [
+                    InlineKeyboardButton(
+                        text=_["H_B_7"],
+                        callback_data="help_callback hb7",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_8"],
+                        callback_data="help_callback hb8",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_9"],
+                        callback_data="help_callback hb9",
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        text=_["H_B_10"],
+                        callback_data="help_callback hb10",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_11"],
+                        callback_data="help_callback hb11",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_12"],
+                        callback_data="help_callback hb12",
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Back",
+                        callback_data="help_page 1",
+                    ),
+                    InlineKeyboardButton(
+                        text="Next",
+                        callback_data="help_page 3",
+                    ),
+                ],
+                mark,
+            ]
+        )
+    
+    elif page == 3:
+        upl = InlineKeyboardMarkup(
             [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_10"],
-                    callback_data="help_callback hb10",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_11"],
-                    callback_data="help_callback hb11",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_12"],
-                    callback_data="help_callback hb12",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_13"],
-                    callback_data="help_callback hb13",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"],
-                    callback_data="help_callback hb14",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
-                ),
-            ],
-            mark,
-        ]
-    )
+                [
+                    InlineKeyboardButton(
+                        text=_["H_B_13"],
+                        callback_data="help_callback hb13",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_14"],
+                        callback_data="help_callback hb14",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_15"],
+                        callback_data="help_callback hb15",
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        text=_["H_B_16"],
+                        callback_data="help_callback hb16",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_17"],
+                        callback_data="help_callback hb17",
+                    ),
+                    InlineKeyboardButton(
+                        text=_["H_B_18"],
+                        callback_data="help_callback hb18",
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Back",
+                        callback_data="help_page 2",
+                    ),
+                ],
+                mark,
+            ]
+        )
+    
     return upl
 
 
